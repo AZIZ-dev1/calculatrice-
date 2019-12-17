79657e90aafb69e912d422356f13f59d96dc9302
AZIZ-dev1 <58938153+AZIZ-dev1@users.noreply.github.com>
2019-12-17 15:18:55 +0100

    Add files via upload

---
+++ calculette.py
@@ -0,0 +1,14 @@
+
+
+def addition(num1, num2):
+    "Cette fonction calcule la somme la difference le produit et le quotient de 2 nombres"
+num1=float(input())
+num2 =float(input())
+S = num1 + num2
+p = num1 * num2
+d = num1 - num2
+q = num1 / num2
+print(num1,num2,S)
+print(num1,num2,p)
+print(num1,num2,d)
+print(num1,num2,q)
